bb23a7bdc38f8174950a3fc776ad99dde592416c
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-08-26 12:14:44 +0530

    small updated

--- BankAPI/settings.py
+++ BankAPI/settings.py
@@ -155,4 +155,4 @@ JWT_AUTH = {
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
